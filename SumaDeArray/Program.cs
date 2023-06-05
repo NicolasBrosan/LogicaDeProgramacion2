2283fb792b303a7ba112dacfd1d93ea8bf4ae277
Nicolas Brosan <nicolasbrosan@hotmail.com>
2023-06-05 10:44:29 -0300

    Add project files.

---
+++ SumaDeArray/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SumaDeArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ar = Console.ReadLine().TrimEnd().Split(',').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+            int result = simpleArraySum(ar);
+
+            Console.WriteLine(result);
+        }
+
+        public static int simpleArraySum(List<int> ar)
+        {
+             return ar.Sum();
+        }
+    }
+}
